b50535a471cc69c9c9fe0618311e84eaee4dd353
Gustaw Grześkowiak <gustaw.grzeskowiak@gmail.com>
2025-06-30 15:44:15 +0200

    cors and api key authorisation

--- FirstAPI/Program.cs
+++ FirstAPI/Program.cs
@@ -4,6 +4,16 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<FirstAPI.Services.UserService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://tester-online.site", "localhost")
+            .WithHeaders("content-type", "X-API-KEY", "Accept")
+            .WithMethods("POST", "GET", "PUT", "DELETE");
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -11,7 +21,21 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+DotNetEnv.Env.Load();
+
+app.Use(async (context, next) =>
+{
+    var apiKey = context.Request.Headers["X-API-KEY"];
+    if (apiKey != Environment.GetEnvironmentVariable("X-API-KEY"))
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Invalid API Key");
+        return;
+    }
+    await next();
+});
+
+app.UseCors();
 
 app.UseAuthorization();
 
